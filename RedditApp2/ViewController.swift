1f15f488c4ea018978c026f62124dc4474ec5f72
SkiDDoUA <antonlena516@gmail.com>
2020-10-29 15:39:52 +0200

    Initial Commit

---
+++ RedditApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditApp2
+//
+//  Created by Anton Kolesnikov on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
